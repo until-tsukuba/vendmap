9c7d48fc978871bdee7d9ac735d14655ddd23f2c
Nakaya <eniehack@outlook.jp>
2025-04-02 00:46:57 +0900

    refactor: darkmodeを設定するボタンをsvelteのcomponent化

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -5,6 +5,7 @@
 	import { VendingMachine } from './vendingMachine';
 	import { writable } from 'svelte/store';
 	import { base } from '$app/paths';
+	import { darkmodeControl } from './darkmodeControl';
 
 	interface Props {
 		here: ml.LngLatLike;
@@ -68,6 +69,7 @@
 				})
 			);
 			map.addControl(new ml.NavigationControl());
+			map.addControl(new darkmodeControl());
 			map.addSource(SOURCE_ID, {
 				type: 'geojson',
 				data: pointData
@@ -170,54 +172,11 @@
 			map.remove();
 		};
 	});
-	$inspect($payment);
 </script>
 
 <div id="map-container">
 	<div id="map" bind:this={mapElem}></div>
 </div>
-
-<button
-	class="absolute top-[13rem] right-2.5 bg-white rounded size-[29px]"
-	onclick={() => {
-		$isDarkmode = !$isDarkmode;
-		if ($isDarkmode) {
-			document.body.className = 'dark';
-		} else {
-			document.body.className = '';
-		}
-	}}
->
-	<span class="grid place-items-center p-1 text-red-200">
-		{#if $isDarkmode}
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="100%"
-				height="100%"
-				fill="#2D7BBE"
-				class="bi bi-moon-fill"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"
-				/>
-			</svg>
-		{:else}
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="100%"
-				height="100%"
-				fill="#ffc300"
-				class="bi bi-sun-fill"
-				viewBox="0 0 16 16"
-			>
-				<path
-					d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"
-				/>
-			</svg>
-		{/if}
-	</span>
-</button>
 
 <div class="absolute bottom-10 left-2 bg-white rounded-lg p-2">
 	<div>
