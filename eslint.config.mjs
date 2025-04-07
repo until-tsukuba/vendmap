5c8426d9ad4d84295c2f6c5b93871e2ffff61533
Nakaya <eniehack@outlook.jp>
2025-04-07 18:54:08 +0900

    chore: eslintのconfigを整理した

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,53 +2,56 @@ import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import svelte from 'eslint-plugin-svelte';
-import svelteParser from 'svelte-eslint-parser';
-
-const tsconfig = tseslint.config({
-	files: ['**/*.js', '**/*.ts', '**/*.svelte'],
-	extends: [
-		js.configs.recommended,
-		...tseslint.configs.strictTypeChecked,
-		...tseslint.configs.stylisticTypeChecked,
-		...svelte.configs['flat/recommended']
-	],
-	ignores: ['svelte.config.js'],
-	languageOptions: {
-		globals: globals.browser,
-		parser: tseslint.parser,
-		parserOptions: {
-			project: true,
-			tsconfigRootDir: import.meta.dirname,
-			extraFileExtensions: ['.svelte']
-		}
-	}
-});
-
-const svelteConfig = tseslint.config({
-	files: ['**/*.svelte'],
-	extends: [...svelte.configs['flat/recommended']],
-	languageOptions: {
-		parser: svelteParser,
-		parserOptions: { parser: tseslint.parser }
-	}
-});
-
-const jsConfig = {
-	files: ['**/*.js'],
-	rules: { '@typescript-eslint/explicit-function-return-type': 'off' }
-};
-
-const configConfig = {
-	files: ['**/*.config.*'],
-	rules: { '@typescript-eslint/naming-convention': 'off' }
-};
+import prettierConfig from 'eslint-config-prettier';
+import svelteConfig from './svelte.config.js';
+import * as espree from 'espree';
 
 export default [
+	js.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
+	...svelte.configs['flat/recommended'],
+	prettierConfig,
+	...svelte.configs['flat/prettier'],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			},
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname
+			}
+		}
+	},
+	{
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		languageOptions: {
+			parserOptions: {
+				extraFileExtensions: ['.svelte'],
+				projectService: true,
+				parser: {
+					ts: tseslint.parser,
+					js: espree
+				},
+				svelteConfig
+			}
+		}
+	},
+	{
+		files: ['**/*.js', '**/*.mjs'],
+		languageOptions: {
+			parserOptions: {
+				parser: espree
+			}
+		}
+	},
+	{
+		files: ['*.config.js'],
+		...tseslint.configs.disableTypeChecked
+	},
 	{
 		ignores: ['.svelte-kit/*', '.vercel/*', 'node_modules']
-	},
-	...tsconfig,
-	...svelteConfig,
-	jsConfig,
-	configConfig
+	}
 ];
